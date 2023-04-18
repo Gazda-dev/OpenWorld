62dc09f805ef4e5a4dda0255278a2020fc143a2e
Gazda-dev <karimzheadshot@gmail.com>
2023-04-18 23:36:15 +0200

    main

---
+++ Source/OpenWorldGame/OpenWorldGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OpenWorldGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OPENWORLDGAME_API AOpenWorldGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
