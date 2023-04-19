72b932e6c1231f70b0bf53b250bf65527f419d47
Gazda-dev <karimzheadshot@gmail.com>
2023-04-19 02:03:06 +0200

    main

---
+++ Source/OpenWorldGame/Public/Items/Item.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Item.generated.h"
+
+UCLASS()
+class OPENWORLDGAME_API AItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+
+	AItem();
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+
+	virtual void BeginPlay() override;
+
+
+	
+
+};
