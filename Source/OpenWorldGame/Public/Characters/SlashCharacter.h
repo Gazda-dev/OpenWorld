bfa29a8a400e6ef29c0646ab690fdc557b271bf6
Gazda-dev <karimzheadshot@gmail.com>
2023-04-27 14:39:57 +0200

    main

---
+++ Source/OpenWorldGame/Public/Characters/SlashCharacter.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "SlashCharacter.generated.h"
+
+UCLASS()
+class OPENWORLDGAME_API ASlashCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	
+	ASlashCharacter();
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	
+	virtual void BeginPlay() override;
+
+
+
+};
