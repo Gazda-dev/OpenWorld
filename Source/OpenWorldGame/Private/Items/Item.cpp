fd0f8b302387c1aae26c72e2266ae22c8d354116
Gazda-dev <karimzheadshot@gmail.com>
2023-04-29 15:50:51 +0200

    main

--- Source/OpenWorldGame/Private/Items/Item.cpp
+++ Source/OpenWorldGame/Private/Items/Item.cpp
@@ -24,6 +24,7 @@ void AItem::BeginPlay()
 	Super::BeginPlay();
 
     Sphere->OnComponentBeginOverlap.AddDynamic(this, &AItem::OnSphereOverlap);
+    Sphere->OnComponentEndOverlap.AddDynamic(this, &AItem::OnSphereEndOverlap);
 }
 
 void AItem::Tick(float DeltaTime)
@@ -44,13 +45,21 @@ float AItem::TransformedCos()
     return Amplitude * FMath::Cos(RunningTime * TimeConstant);
 }
 
-void AItem::OnSphereOverlap(UPrimitiveComponent OnComponentBeginOverlap, UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, 
+void AItem::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, 
                             UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
     const FString OtherActorName = OtherActor->GetName();
     if (GEngine)
     {
         GEngine->AddOnScreenDebugMessage(1, 30.f, FColor::Red, OtherActorName);
+    }
+}
+
+void AItem::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+    if (GEngine)
+    {
+        GEngine->AddOnScreenDebugMessage(1, 30.f, FColor::Green, TEXT("End overlapping"));
     }
 }
 
