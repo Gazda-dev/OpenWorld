398faeb24bcd3e4f56a4719993559281c5fc10b9
Gazda-dev <karimzheadshot@gmail.com>
2023-05-04 00:14:08 +0200

    main

---
+++ Source/OpenWorldGame/Public/Enemy/Enemy.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Enemy.generated.h"
+
+UCLASS()
+class OPENWORLDGAME_API AEnemy : public ACharacter
+{
+	GENERATED_BODY()
+
+	virtual void Tick(float DeltaTime) override;
+
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+public:
+
+	AEnemy();
+
+protected:
+
+	virtual void BeginPlay() override;
+
+public:	
+
+
+};
