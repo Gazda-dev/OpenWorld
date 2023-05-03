8ea2742a10e503ce60446665290803d587ad7bb6
Gazda-dev <karimzheadshot@gmail.com>
2023-05-03 15:12:03 +0200

    main

--- Source/OpenWorldGame/Public/Items/Weapons/Weapon.h
+++ Source/OpenWorldGame/Public/Items/Weapons/Weapon.h
@@ -23,16 +23,26 @@ public:
     void Equip(USceneComponent* InParent, FName InSocketName);
     void AttachMeshToSocket(USceneComponent* InParent, const FName& InSocketName);
 protected:
+    virtual void BeginPlay() override;
 
     virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
                                  bool bFromSweep, const FHitResult& SweepResult) override;
 
     virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
 
+    UFUNCTION()
+    void OnBoxOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+            bool bFromSweep, const FHitResult& SweepResult); 
 private:
     UPROPERTY(EditAnywhere, Category = "Weapon Properties cpp")
     USoundBase* EquipSound;
 
     UPROPERTY(EditAnywhere, Category = "Weapon Properties cpp")
     UBoxComponent* WeaponBox;
+
+    UPROPERTY(VisibleAnywhere)
+    USceneComponent* BoxTraceStart;
+
+    UPROPERTY(VisibleAnywhere)
+    USceneComponent* BoxTraceEnd;
 };
